2fff7f1984accad0eac5e4f286d20e54f00a25c4
katsumiexe <counterpost2016@gmail.com>
2022-07-18 21:39:48 +0900

    ar

--- first_sheet.php
+++ first_sheet.php
@@ -33,8 +33,8 @@ input,select{
 }
 
 .td_title{
-	background		:#e0e0ff;
-	color			:#303030;
+	background		:#000090;
+	color			:#fafafa;
 	font-size		:16px;
 	height			:30px;
 	line-height		:30px;
@@ -45,12 +45,21 @@ input,select{
 	display			:block;
 	font-size		:14px;
 	padding-left	:10px;
-	width			:80px;
 	font-weight		:700;
 	height			:20px;
 	line-height		:22px;
 }
 
+.ck_title{
+	margin			:1px 10px;
+	background		:#906000;
+	color			:#fafafa;
+	height			:24px;
+	line-height		:24px;
+	font-size		:15px;
+	font-weight		:700;
+	text-align		:center;
+}
 
 
 .ck_box0{
@@ -62,12 +71,13 @@ input,select{
 	height			:30px;
 	line-height		:30px;
 	margin			:5px;
-	width			:110px;
+	width			:150px;
 }
 
 .ck_box0:checked + .check1{
 	display				:inline-block;
 }
+
 
 .ck_box3{
 	display				:none;
@@ -100,6 +110,34 @@ input,select{
 	height				:16px;
 	border				:2px solid #303030;
 	background			:#fafafa;
+}
+
+.ques{
+	display			:inline-block;
+	width			:18px;
+	height			:18px;
+	line-height		:18px;
+	text-align		:center;
+	position		:absolute;
+	top				:0;
+	right			:5px;
+
+}
+
+.ans{
+	display			:none;
+	position		:absolute;
+	top				:20px;
+	right			:-5px;
+	padding			:3px;
+	font-size		:13px;
+	background		:#fff0e0;
+	border			:1px solid #202020;
+	color			:#202020;	
+	line-height		:17px;
+	width			:120px;
+	z-index			:3;
+	text-align:left;
 }
 
 .w100{
@@ -230,12 +268,15 @@ input,select{
 
 	<tr>
 		<td>
+
+<div class="ck_title">CMSコンテンツ</div>
 			<label for="check_1" class="ck_box0">
 				<span class="ck_box1">
 					<input id="check_1" type="checkbox" class="ck_box2" value="1" <?if($dat["check_1"] ==1){?>checked="checked"<?}?>>
 					<span class="ck_box3"></span>
 				</span>
-				<span>お知らせ</span>
+				<span>Notice</span>
+				<span class="ques">●<span class="ans">店舗からキャストへ連絡事項の告知をするためのものです。</span></span>
 			</label>
 
 			<label for="check_2" class="ck_box0">
@@ -244,6 +285,7 @@ input,select{
 					<span class="ck_box3"></span>
 				</span>
 				<span>EasyTalk</span>
+				<span class="ques">●<span class="ans">キャストが顧客へ営業メールを送るためのものです。</span></span>
 			</label>
 
 			<label for="check_3" class="ck_box0">
@@ -252,16 +294,20 @@ input,select{
 					<span class="ck_box3"></span>
 				</span>
 				<span>スケジュール</span>
+				<span class="ques">●<span class="ans">キャストの出勤日、時間を表示するためのものです。</span></span>
 			</label>
 		</td>
 
 		<td>
+<div class="ck_title">サイト内ページ</div>
+
 			<label for="check_4" class="ck_box0">
 				<span class="ck_box1">
 					<input id="check_4" type="checkbox" class="ck_box2" value="1" <?if($dat["check_1"] ==1){?>checked="checked"<?}?>>
 					<span class="ck_box3"></span>
 				</span>
 				<span>TOP挨拶</span>
+				<span class="ques">●<span class="ans">ページのTOPに挨拶の一文を入れるためのものです。</span></span>
 			</label>
 			<label for="check_5" class="ck_box0">
 				<span class="ck_box1">
@@ -269,46 +315,7 @@ input,select{
 					<span class="ck_box3"></span>
 				</span>
 				<span>ニュース</span>
-			</label>
-
-			<label for="check_6" class="ck_box0">
-				<span class="ck_box1">
-					<input id="check_6" type="checkbox" class="ck_box2" value="1" <?if($dat["check_3"] ==1){?>checked="checked"<?}?>>
-					<span class="ck_box3"></span>
-				</span>
-				<span>アクセス</span>
-			</label>
-
-			<label for="check_7" class="ck_box0">
-				<span class="ck_box1">
-					<input id="check_7" type="checkbox" class="ck_box2" value="1" <?if($dat["check_3"] ==1){?>checked="checked"<?}?>>
-					<span class="ck_box3"></span>
-				</span>
-				<span>リクルート</span>
-			</label>
-
-			<label for="check_8" class="ck_box0">
-				<span class="ck_box1">
-					<input id="check_8" type="checkbox" class="ck_box2" value="1" <?if($dat["check_3"] ==1){?>checked="checked"<?}?>>
-					<span class="ck_box3"></span>
-				</span>
-				<span>プライバシー</span>
-			</label>
-
-			<label for="check_9" class="ck_box0">
-				<span class="ck_box1">
-					<input id="check_9" type="checkbox" class="ck_box2" value="1" <?if($dat["check_3"] ==1){?>checked="checked"<?}?>>
-					<span class="ck_box3"></span>
-				</span>
-				<span>キャストブログ</span>
-			</label>
-
-			<label for="check_10" class="ck_box0">
-				<span class="ck_box1">
-					<input id="check_10" type="checkbox" class="ck_box2" value="1" <?if($dat["check_3"] ==1){?>checked="checked"<?}?>>
-					<span class="ck_box3"></span>
-				</span>
-				<span>スタッフブログ</span>
+				<span class="ques">●<span class="ans">イベントやキャストに関する様々なお知らせを告知するためのものです。</span></span>
 			</label>
 
 			<label for="check_11" class="ck_box0">
@@ -317,38 +324,87 @@ input,select{
 					<span class="ck_box3"></span>
 				</span>
 				<span>システム</span>
+				<span class="ques">●<span class="ans">料金など、お店のシステムを告知するためのものです。</span></span>
 			</label>
 
+			<label for="check_9" class="ck_box0">
+				<span class="ck_box1">
+					<input id="check_9" type="checkbox" class="ck_box2" value="1" <?if($dat["check_3"] ==1){?>checked="checked"<?}?>>
+					<span class="ck_box3"></span>
+				</span>
+				<span>キャストブログ</span>
+				<span class="ques">●<span class="ans">キャストがHIME-Karteより投稿するブログです。</span></span>
+			</label>
 
+			<label for="check_10" class="ck_box0">
+				<span class="ck_box1">
+					<input id="check_10" type="checkbox" class="ck_box2" value="1" <?if($dat["check_3"] ==1){?>checked="checked"<?}?>>
+					<span class="ck_box3"></span>
+				</span>
+				<span>スタッフブログ</span>
+				<span class="ques">●<span class="ans">スタッフが投稿するブログです。</span></span>
+			</label>
+
+			<label for="check_6" class="ck_box0">
+				<span class="ck_box1">
+					<input id="check_6" type="checkbox" class="ck_box2" value="1" <?if($dat["check_3"] ==1){?>checked="checked"<?}?>>
+					<span class="ck_box3"></span>
+				</span>
+				<span>アクセス</span>
+				<span class="ques">●<span class="ans">お店の住所、連絡先、MAPを表示します。</span></span>
+			</label>
+
+			<label for="check_7" class="ck_box0">
+				<span class="ck_box1">
+					<input id="check_7" type="checkbox" class="ck_box2" value="1" <?if($dat["check_3"] ==1){?>checked="checked"<?}?>>
+					<span class="ck_box3"></span>
+				</span>
+				<span>リクルート</span>
+				<span class="ques">●<span class="ans">キャストの求人受付です。</span></span>
+			</label>
+
+			<label for="check_8" class="ck_box0">
+				<span class="ck_box1">
+					<input id="check_8" type="checkbox" class="ck_box2" value="1" <?if($dat["check_3"] ==1){?>checked="checked"<?}?>>
+					<span class="ck_box3"></span>
+				</span>
+				<span>プライバシーポリシー</span>
+				<span class="ques">●<span class="ans">サイトとしての個人情報への取り組みを明記するためのものです。</span></span>
+			</label>
 
 		</td>
 		<td>
+<div class="ck_title">システムコンテンツ</div>
+			<label for="check_12" class="ck_box0">
+				<span class="ck_box1">
+					<input id="check_12" type="checkbox" class="ck_box2" value="1" <?if($dat["check_3"] ==1){?>checked="checked"<?}?>>
+					<span class="ck_box3"></span>
+				</span>
+				<span>アナリティクス</span>
+				<span class="ques">●<span class="ans">サイトへのアクセス状況を調べるためのものです。</span></span>
+			</label>
 
+			<label for="check_13" class="ck_box0">
+				<span class="ck_box1">
+					<input id="check_13" type="checkbox" class="ck_box2" value="1" <?if($dat["check_3"] ==1){?>checked="checked"<?}?>>
+					<span class="ck_box3"></span>
+				</span>
+				<span>サーチコンソール</span>
+				<span class="ques">●<span class="ans">検索評価の確認をするためのものです。</span></span>
+			</label>
 
-
-
+			<label for="check_14" class="ck_box0">
+				<span class="ck_box1">
+					<input id="check_14" type="checkbox" class="ck_box2" value="1" <?if($dat["check_3"] ==1){?>checked="checked"<?}?>>
+					<span class="ck_box3"></span>
+				</span>
+				<span>ファビコン</span>
+				<span class="ques">●<span class="ans">ブラウザのタイトルタブに自身のアイコンを表示します。</span></span>
+			</label>
 		</td>
 	</tr>
 </table>
 
-
-▼使用コンテンツ
-
-□
-□
-□
-□アクセス
-□リクルート
-□キャストブログ
-□スタッフブログ
-□プライバシーポリシー
-
-□Google アナリティクス
-□Google サーチコンソール
-
-▼使用キャスト情報
-プロフィール
-オプション
 
 </body>
 </html>
