caa06d34a3f0bf70286fa7104b637f4d0f7fba9b
katsumiexe <57611034+katsumiexe@users.noreply.github.com>
2022-07-20 08:56:33 +0900

    ar

--- first_sheet.php
+++ first_sheet.php
@@ -392,8 +392,8 @@ input,select{
 			<span class="td_tag">本文</span>
 			<textarea id="rec_0" class="textarea2 w360"></textarea>
 			<span class="td_tag">確認項目</span>
-			<input id="rec_1" type="text" class="textbox w300"><label class="nese">必須</label>
-			<input id="rec_2" type="text" class="textbox w300"><label class="nese">必須</label>
+			<input id="rec_1" type="text" class="textbox w300" placeholder="お名前"><label class="nese">必須</label>
+			<input id="rec_2" type="text" class="textbox w300" placeholder="ご住所"><label class="nese">必須</label>
 			<input id="rec_3" type="text" class="textbox w300"><label class="nese">必須</label>
 			<input id="rec_4" type="text" class="textbox w300"><label class="nese">必須</label>
 			<input id="rec_5" type="text" class="textbox w300"><label class="nese">必須</label>
@@ -403,6 +403,33 @@ input,select{
 		</td>
 	</tr>
 </table>
+
+<table class="table_3">
+	<tr>
+		<td class="td_title" colspan="3">プライバシーポリシー</td>
+	</tr>
+	<tr>
+		<td>
+			<span class="td_tag">本文</span>
+			<textarea id="rec_0" class="textarea2 w360">
+			『○○』（以下，「当社」といいます。）は，本ウェブサイト上で提供するサービス（以下,「本サービス」といいます。）における，ユーザーの個人情報の取扱いについて，以下のとおりプライバシーポリシー（以下，「本ポリシー」といいます。）を定めます。
+第1条　個人情報
+「個人情報」とは，個人情報保護法にいう「個人情報」を指すものとし，生存する個人に関する情報であって，当該情報に含まれる氏名，生年月日，住所，電話番号，連絡先その他の記述等により特定の個人を識別できる情報及び容貌，指紋，声紋にかかるデータ，及び健康保険証の保険者番号などの当該情報単体から特定の個人を識別できる情報（個人識別情報）を指します。
+第2条　個人情報の収集方法
+当社は，ユーザーが利用登録をする際に氏名，生年月日，住所，電話番号，メールアドレス，銀行口座番号，クレジットカード番号，運転免許証番号などの個人情報をお尋ねすることがあります。また，ユーザーと提携先などとの間でなされたユーザーの個人情報を含む取引記録や決済に関する情報を,当社の提携先（情報提供元，広告主，広告配信先などを含みます。以下，｢提携先｣といいます。）などから収集することがあります。
+第3条　個人情報の利用目的
+当社が個人情報を収集・利用する目的は，以下のとおりです。 当社サービスの提供・運営のため
+ユーザーからのお問い合わせに回答するため（本人確認を行うことを含む）
+ユーザーが利用中のサービスの新機能，更新情報，キャンペーン等及び当社が提供する他のサービスの案内のメールを送付するため
+メンテナンス，重要なお知らせなど必要に応じたご連絡のため
+利用規約に違反したユーザーや，不正・不当な目的でサービスを利用しようとするユーザーの特定をし，ご利用をお断りするため
+ユーザーにご自身の登録情報の閲覧や変更，削除，ご利用状況の閲覧を行っていただくため
+			
+			</textarea>
+		</td>
+	</tr>
+</table>
+
 
 <table class="table_3">
 	<tr>
